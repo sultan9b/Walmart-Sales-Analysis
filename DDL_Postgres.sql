6e1e1cc13f3a0becd1566747a85e23d9b89d5bfa
sultan9b <sbuzhkenov@gmail.com>
2026-02-26 11:16:02 +0500

    chore: update DDL, created dm schema

--- DDL_Postgres.sql
+++ DDL_Postgres.sql
@@ -100,4 +100,76 @@ foreign key (seller_id) references raw.sellers(seller_id);
 -- payments -> orders
 alter table raw.payments add constraint fk_payments_order
 foreign key (order_id) references raw.orders(order_id);		
-	
+
+
+-- Одна главная витрина с уже готовыми JOIN
+create table dm.sales as
+select 
+    o.order_id,
+    o.order_purchase_timestamp,
+    o.order_status,
+    p.product_category,
+    c.customer_id,
+    c.customer_city,
+    c.customer_state,
+    s.seller_city,
+    s.seller_state,
+    oi.price,
+    oi.freight_value,
+    pay.payment_type,
+    pay.payment_value
+from raw.orders o
+join raw.customers c on o.customer_id = c.customer_id
+join raw.order_items oi on o.order_id = oi.order_id
+join raw.products p on oi.product_id = p.product_id
+join raw.sellers s on oi.seller_id = s.seller_id
+join raw.payments pay on o.order_id = pay.order_id;
+
+-- Витрина о логистике
+create table dm.delivery as 
+	select o.order_id,
+		   s.seller_id,
+		   s.seller_state,
+		   s.seller_city,
+		   o.order_delivered_carrier_date,
+		   c.customer_id,
+		   c.customer_state,
+		   c.customer_city,
+		   o.order_delivered_customer_date,
+		   extract(day from (o.order_delivered_customer_date - o.order_delivered_carrier_date)) as delivery_days,
+		   extract(hour from (o.order_delivered_customer_date - o.order_delivered_carrier_date)) as delivery_hours,
+		   concat(extract(day from (o.order_delivered_customer_date - o.order_delivered_carrier_date)), ' days ',
+	    		extract(hour from (o.order_delivered_customer_date - o.order_delivered_carrier_date)), ' hours')
+	       as delivery_time
+	from raw.customers c
+	join raw.orders o using(customer_id)
+	join raw.order_items oi using(order_id)
+	join raw.sellers s using(seller_id)
+	where o.order_delivered_customer_date is not null
+		  and o.order_delivered_carrier_date is not null; 
+
+-- Витрина о покупательских способностях
+create table dm.purchasing_power as
+	select c.customer_id,
+		   c.customer_unique_id,
+		   c.customer_state,
+		   c.customer_city,
+		   p.payment_type,
+		   p.payment_installments,
+		   p.payment_value,
+		   o.order_id,
+		   o.order_purchase_timestamp
+	from raw.customers c
+	join raw.orders o using(customer_id)
+	join raw.payments p using(order_id)
+	where o.order_status = 'delivered';
+
+
+
+
+
+
+
+
+
+
